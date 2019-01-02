bb5efff10fd5773cd02b5e10bbe981b3715f3f0a
Chrolan <jordikleriga@gmail.com>
2019-01-02 14:03:41 -0600

    fixed html landing page

--- src/components/workout.js
+++ src/components/workout.js
@@ -1,24 +1,23 @@
 import React from 'react';
 import {connect} from 'react-redux';
-import Exercise from './exercises'
+//import Exercise from './exercises'
 
 export class Workout extends React.Component {
 
      render() {
 
-        const workout = this.props.workoutList[this.props.workoutList.index];
+        /*const workout = this.props.workoutList[this.props.workoutList.index];
         const listOfExercises = workout.exercises.map((exercise, index) => {
             return (
                 <Exercise {...exercise} key={index}/>
             )}
-        );
+        );*/
 
          return(
              <div className="workout">
                  <span className="workout-name">{this.props.name}</span>
                  <span className="workout-date">{this.props.date}</span>
                  <div className="exercises">
-                     {listOfExercises}
                  </div>
              </div>
 
