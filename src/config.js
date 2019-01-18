bd0431b7cee8a77d0a0ee265322989716a43c6c6
Jordi Kleriga <jordikleriga@gmail.com>
2019-01-18 00:25:18 -0600

    added remote URL

--- src/config.js
+++ src/config.js
@@ -1,2 +1,2 @@
 export const API_BASE_URL =
-    process.env.REACT_APP_API_BASE_URL || 'http://localhost:8080/workouts';
+    process.env.REACT_APP_API_BASE_URL || 'https://nameless-shore-82289.herokuapp.com/';
